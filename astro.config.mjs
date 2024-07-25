7216671b421ac9ad17239e419299a6b042bd83fd
Artem <34752993+a-rebets@users.noreply.github.com>
2024-07-25 19:09:24 +0000

    added node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
-    enabled: false,
+    enabled: false
   },
   output: "server",
   integrations: [db(), tailwind()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
